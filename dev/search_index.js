b4d35340b6384e0d35dcb630544d4078dc4087bf
Documenter.jl <documenter@juliadocs.github.io>
2024-05-20 08:36:00 +0000

    build based on 361c388

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = ABLR_2022","category":"page"},{"location":"#ABLR_2022","page":"Home","title":"ABLR_2022","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for ABLR_2022.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [ABLR_2022]","category":"page"}]
+}
