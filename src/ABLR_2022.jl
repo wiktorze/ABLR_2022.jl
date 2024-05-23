03819610f29c90d7749c3e497b052b0b43c32f6d
Wiktor Zieliński <120274586+wiktorze@users.noreply.github.com>
2024-05-23 10:58:49 +0200

    trying the module

--- src/ABLR_2022.jl
+++ src/ABLR_2022.jl
@@ -1,6 +1,6 @@
 module ABLR_2022
 using JLD2, MAT, Statistics, JuMP, Ipopt, PrettyTables, GLM, Plots, DataFrames, CSV
-export compile
+export results
 include("t1.jl")
 include("t2.jl")
 include("simulation.jl")
@@ -9,7 +9,7 @@ include("t3.jl")
 include("fig1.jl")
 include("fig2.jl")
 
-function compile()
+function results()
     t1(latex = false)
     t2(latex = false)
     t3(latex = false)  
